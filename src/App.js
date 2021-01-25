6180211aee7e83201c13fa9c03161ee847885ad8
Gabriel Miroiu <miroiugabriel1231@gmail.com>
2021-01-25 21:59:58 +0200

    Added versioning

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './style.css';
 import TodoApp from './components/TodoApp';
 
+const version = Number.parseFloat(localStorage.getItem('version') || 0);
+const newVersion = 1.1;
+
+if (version < newVersion) {
+	console.log('Outdated app: removing local storage');
+	localStorage.removeItem('todos');
+}
+
+localStorage.setItem('version', JSON.stringify(newVersion));
+
 export default function App() {
 	return <TodoApp />;
 }
